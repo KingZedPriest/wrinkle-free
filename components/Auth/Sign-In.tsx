e4199b6553d91afc838fa5b96d2f6557318a7965
Priest <Charleschukwuemeka47@gmail.com>
2024-09-03 12:54:14 +0100

    Feat: Add the Sign-in component

---
+++ components/Auth/Sign-In.tsx
@@ -0,0 +1,12 @@
+const SignIn = () => {
+    return (
+        <main className="flex">
+            <div className="w-full lg:w-1/2 p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12 h-dvh flex items-center justify-center">
+                <h1>This is a sample</h1>
+            </div>
+            <div className="hidden lg:block lg:w-1/2 bg-generalBlue"></div>
+        </main>
+    );
+}
+
+export default SignIn;
