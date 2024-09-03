b47a9f3767a9accef4a21e9743f3d1aed2a95904
Priest <Charleschukwuemeka47@gmail.com>
2024-09-03 13:41:57 +0100

    Feat: Add the Auth schema

--- components/Auth/Sign-In.tsx
+++ components/Auth/Sign-In.tsx
@@ -6,8 +6,10 @@ import logo from "../../public/logo.png";
 const SignIn = () => {
     return (
         <main className="flex">
-            <div className="w-full lg:w-1/2 p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12 h-dvh flex items-center justify-center">
-                <h1>This is a sample</h1>
+            <div className="w-full lg:w-1/2 h-dvh flex items-center justify-center">
+                <div className="bg-light-600 dark:bg-dark-600 border border-slate-200 dark:border-slate-800 p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12 rounded-lg">
+
+                </div>
             </div>
             <div className="hidden lg:flex lg:w-1/2 bg-generalBlue dark:bg-cloudBlue items-center justify-center">
                 <div className="rounded-[50%] p-8 bg-cloudBlue dark:bg-generalBlue">
