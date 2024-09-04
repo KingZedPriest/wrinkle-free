85968cb7412f6a138b5a0d02e9d4e8c373566c7d
Priest <Charleschukwuemeka47@gmail.com>
2024-09-05 00:23:18 +0100

    Feat: Push the log out to the bottom

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -2,36 +2,40 @@
 
 import { usePathname } from "next/navigation";
 
-//Components
+//Component
 import SideItem from "./SideItem";
 
 //Icons
 import { AddCircle, HomeHashtag, Logout, Profile2User, ProfileCircle, SearchNormal } from "iconsax-react";
 
 const navItems = [
-    { href: "/dashboard", icon: HomeHashtag, label: "Home" },
-    { href: "/orders", icon: AddCircle, label: "Orders" },
-    { href: "/search", icon: SearchNormal, label: "Search" },
-    { href: "/staff", icon: Profile2User, label: "Staff", role: "super_admin" },
-    { href: "/profile", icon: ProfileCircle, label: "Profile" },
-    { href: "/logout", icon: Logout, label: "Logout" }
+  { href: "/dashboard", icon: HomeHashtag, label: "Home" },
+  { href: "/orders", icon: AddCircle, label: "Orders" },
+  { href: "/search", icon: SearchNormal, label: "Search" },
+  { href: "/staff", icon: Profile2User, label: "Staff", role: "super_admin" },
+  { href: "/profile", icon: ProfileCircle, label: "Profile" },
 ];
 
+const logoutItem = { href: "/logout", icon: Logout, label: "Logout" };
+
 const SideBar = ({ role }: { role: string }) => {
-    const currentPath = usePathname();
+  const currentPath = usePathname();
 
-    // Filter navItems based on role
-    const filteredNavItems = navItems.filter(item => !item.role || item.role === role);
+  // Filter navItems based on role
+  const filteredNavItems = navItems.filter(item => !item.role || item.role === role);
 
-    return (
-        <main className="px-10 py-6 dark:bg-light-600 bg-dark-600 w-[20rem] text-textDark dark:text-textLight h-dvh rounded-r-2xl">
-            <div className="flex flex-col gap-y-8 mt-20">
-                {filteredNavItems.map((item, index) => (
-                    <SideItem key={index} currentPath={currentPath} href={item.href} icon={item.icon} label={item.label} />
-                ))}
-            </div>
-        </main>
-    );
-}
+  return (
+    <main className="px-10 py-6 dark:bg-light-600 bg-dark-600 w-[20rem] text-textDark dark:text-textLight h-screen flex flex-col rounded-r-2xl backdrop-blur-md bg-opacity-30">
+      <div className="flex-grow flex flex-col gap-y-8 mt-20">
+        {filteredNavItems.map((item, index) => (
+          <SideItem key={index} currentPath={currentPath} href={item.href} icon={item.icon} label={item.label} />
+        ))}
+      </div>
+      <div className="mt-8">
+        <SideItem currentPath={currentPath} href={logoutItem.href} icon={logoutItem.icon} label={logoutItem.label} />
+      </div>
+    </main>
+  );
+};
 
 export default SideBar;
--- components/SideItem.tsx
+++ components/SideItem.tsx
@@ -1,18 +1,21 @@
 import Link from "next/link";
 
 const SideItem = ({ href, icon: Icon, currentPath, label }: NavItem) => {
+  const isActive = currentPath === href;
+  
+  // Common classes for glass effect
+  const glassEffect = "backdrop-blur-md bg-opacity-30";
+  
+  // Active and base classes
+  const baseClasses = `dark:hover:bg-dark-600 hover:bg-light-600 dark:hover:text-white hover:text-black duration-300 ${glassEffect}`;
+  const activeClasses = `dark:bg-dark-600 bg-light-600 dark:text-white text-black ${glassEffect}`;
 
-    const isActive = currentPath === href;
-    const baseClasses = "dark:hover:bg-dark-600 hover:bg-light-600 dark:hover:text-white hover:text-black duration-300";
-    const activeClasses = "dark:bg-dark-600 bg-light-600 dark:text-white text-black";
-
-
-    return (
-        <Link href={href} className={`${isActive ? activeClasses : baseClasses } w-full rounded-[2rem] flex gap-x-2 py-3 px-6 items-center`}>
-            <Icon size="24" variant={isActive ? "Bold" : "Outline"} />
-            <p className="font-semibold">{label}</p>
-        </Link>
-    );
-}
+  return (
+    <Link href={href} className={`${isActive ? activeClasses : baseClasses} w-full rounded-[2rem] flex gap-x-2 py-3 px-6 items-center`}>
+      <Icon size="24" variant={isActive ? "Bold" : "Outline"} />
+      <p className="font-semibold">{label}</p>
+    </Link>
+  );
+};
 
 export default SideItem;
