d1031d3faddb3d10f479b07f8709a0c5e0425507
Priest <Charleschukwuemeka47@gmail.com>
2024-09-04 19:11:00 +0100

    Feat: Restrict access to some pages

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
     return (
         <section>
             {children}
-            <section className="md:hidden"><DownBar role={"admin"}/></section>
+            <section className="md:hidden"><DownBar role={"super_admin"}/></section>
         </section>
     )
 }
