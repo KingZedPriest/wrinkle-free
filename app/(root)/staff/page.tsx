d4c1aca06a53ddc7900b5b866ef35830897bbfa3
Priest <Charleschukwuemeka47@gmail.com>
2024-09-07 14:42:33 +0100

    Feat: Add the new staff page

--- app/(root)/staff/page.tsx
+++ app/(root)/staff/page.tsx
@@ -1,7 +1,11 @@
+//Components
+import StaffHeader from "@/components/Staff/StaffHeader";
+
+
 const page = () => {
     return ( 
-        <main>
-            
+        <main className="py-5 mb-20 lg:mb-10">
+            <StaffHeader totalStaff={10} />
         </main>
      );
 }
--- components/Staff/StaffHeader.tsx
+++ components/Staff/StaffHeader.tsx
@@ -7,11 +7,11 @@ const StaffHeader = ({ totalStaff }: { totalStaff: number }) => {
     return (
         <main className="flex justify-between items-center">
             <div className="flex gap-x-2 items-center">
-                <Android size="20" className="text-textOrange bg-textOrange/30 p-1" />
+                <Android size="32" className="text-textOrange bg-textOrange/30 p-1" />
                 <p className="text-xl md:text-2xl xl:text-3xl font-semibold dark:text-white text-black">{totalStaff}</p>
                 <p>Workers</p>
             </div>
-            <Link href="/staff/new" className="bg-generalBlue dark:bg-cloudBlue p-3 rounded-[2rem] hover:bg-blue-800 duration-300">Add Staff</Link>
+            <Link href="/staff/new" className="bg-generalBlue dark:bg-cloudBlue px-6 py-3 rounded-[2rem] hover:bg-blue-800 dark:hover:bg-blue-400 duration-300 text-white">Add Staff</Link>
         </main>
     );
 }
