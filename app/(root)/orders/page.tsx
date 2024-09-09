af00fac8d5099a52c3cb57f42107df0ed770ca99
Priest <Charleschukwuemeka47@gmail.com>
2024-09-09 19:36:53 +0100

    Feat: Done with the orders

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -1,8 +1,10 @@
 //Actions
 import { getCurrentUser } from "@/actions/fetch/currentUser";
 
+
 //Components
 import ScrollReveal from "@/components/RevelOnScroll";
+import OrderHeader from "@/components/Orders/OrderHeader";
 
 
 export const dynamic = 'force-dynamic';
@@ -13,7 +15,7 @@ const page = async () => {
 
     return ( 
         <main className="py-5 mb-20 lg:mb-10">
-            
+            <OrderHeader totalOrders={20}/>
         </main>
      );
 }
--- components/Orders/OrderHeader.tsx
+++ components/Orders/OrderHeader.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 //Icons
-import { ShoppingBag } from "iconsax-react";
+import { Bag2 } from "iconsax-react";
 
 const OrderHeader = ({ totalOrders }: { totalOrders: number }) => {
     return ( 
@@ -9,9 +9,9 @@ const OrderHeader = ({ totalOrders }: { totalOrders: number }) => {
             <div className="flex gap-x-2 items-center">
                 <p className="text-xl md:text-2xl xl:text-3xl font-semibold dark:text-white text-black">{totalOrders}</p>
                 <p>Orders</p>
-                <ShoppingBag size="24" className="text-textGreen" variant="Bold" />
+                <Bag2 size="30" className="text-textGreen" variant="Bold" />
             </div>
-            <Link href="/orders/new" className="bg-generalBlue dark:bg-cloudBlue px-6 md:px-8 xl:px-10 py-3 rounded-[2rem] hover:bg-blue-800 dark:hover:bg-blue-400 duration-300 text-white">Add Staff</Link>
+            <Link href="/orders/new" className="bg-generalBlue dark:bg-cloudBlue px-6 md:px-8 xl:px-10 py-3 rounded-[2rem] hover:bg-blue-800 dark:hover:bg-blue-400 duration-300 text-white">New Order</Link>
         </main>
      );
 }
