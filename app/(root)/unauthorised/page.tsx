5621f45f0b1199d4e53ce7852458e36b11cc354d
Priest <Charleschukwuemeka47@gmail.com>
2024-09-09 19:31:44 +0100

    Feat: Finish the unauthorised page

--- app/(root)/unauthorised/page.tsx
+++ app/(root)/unauthorised/page.tsx
@@ -6,8 +6,8 @@ import { Ban } from "lucide-react";
 const page = () => {
     return (
         <main className="h-dvh flex flex-col gap-y-10 items-center justify-center">
-            <Ban className="text-textRed size-10" />
-            <p className="text-lg sm:text-xl md:text-2xl xl:text-3xl font-bold">Unauthorised</p>
+            <Ban className="text-textRed size-14" strokeWidth={4} />
+            <p className="text-lg sm:text-xl md:text-2xl xl:text-3xl font-bold">Access Denied</p>
             <Link href="/dashboard" className="bg-generalBlue dark:bg-cloudBlue px-6 md:px-8 xl:px-10 py-3 rounded-[2rem] hover:bg-blue-800 dark:hover:bg-blue-400 duration-300 text-white">Return to Dashboard</Link>
         </main>
     );
