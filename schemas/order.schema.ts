eee37bdeac539834aeb47586f49518938d0e54a1
Priest <Charleschukwuemeka47@gmail.com>
2024-09-10 11:56:36 +0100

    Fix: Edit the schema, add invalid_type_error

--- schemas/order.schema.ts
+++ schemas/order.schema.ts
@@ -6,15 +6,15 @@ export const OrderSchema = z
         notes: z
             .string().optional(),
         price: z
-            .number({ required_error: "Price is needed" }).positive(),
+            .number({ required_error: "Price is needed", invalid_type_error: "Kindly enter a number" }).positive({message: "Only positive numbers are accepted"}),
         amountPaid: z
-            .number({ required_error: "Amount paid is required" }).positive(),
+            .number({ required_error: "Amount paid is required", invalid_type_error: "Kindly enter a number" }).positive({message: "Only positive numbers are accepted"}),
         pickupDay: z
-            .date({ required_error: "The pick up day is required" }),
+            .date({ required_error: "The pick up day is required", invalid_type_error: "Kindly chose a date" }),
         quantity: z
-            .number({ required_error: "The total number of clothes is required" }).positive(),
+            .number({ required_error: "The total number of clothes is required", invalid_type_error: "Kindly enter a number" }).positive({message: "Only positive numbers are accepted"}),
         service: z
-            .string({ required_error: "Kindly specify the laundry service for clothes" })
+            .string({ required_error: "Kindly specify the laundry service for clothes", invalid_type_error: "Kindly enter a word or a sentence" })
     });
 
 export type Order = z.infer<typeof OrderSchema>;
