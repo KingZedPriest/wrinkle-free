f93437f217fef96d4f944a940e4eafedd9345a78
Priest <Charleschukwuemeka47@gmail.com>
2024-09-11 07:33:38 +0100

    Feat: Add the generate file name function

--- lib/fileUpload.ts
+++ lib/fileUpload.ts
@@ -1,3 +1,5 @@
+import crypto from "crypto"
+
 export const computeSHA256 = async (file: File) => {
     const buffer = await file.arrayBuffer();
     const hashBuffer = await crypto.subtle.digest("SHA-256", buffer);
@@ -5,3 +7,5 @@ export const computeSHA256 = async (file: File) => {
     const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
     return hashHex;
 }
+
+export const generateFileName = (bytes = 16) => crypto.randomBytes(bytes).toString("hex");
