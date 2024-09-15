0bbabcc9cb9089394731ddded3562a850269801a
Priest <Charleschukwuemeka47@gmail.com>
2024-09-15 11:39:46 +0100

    Fix: Fix the error

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: { serverComponentsExternalPackages: [ '@aws-sdk/client-s3', '@aws-sdk/s3-request-presigner' ] },
   images: {
     remotePatterns: [
       {
